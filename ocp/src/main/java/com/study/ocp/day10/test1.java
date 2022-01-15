b21b5ff8bb7cfdfd5e3078432100853277fbf9dd
jamesliao <JamesCoding888@hotmail.com>
2022-01-15 16:35:00 +0800

    JavaOCP_江_20211023

--- ocp/src/main/java/com/study/ocp/day10/test1.java
+++ ocp/src/main/java/com/study/ocp/day10/test1.java
@@ -15,22 +15,39 @@ public class test1 {
 		{
 			x[n]=100;//new ArrayIndexOutOfBoundsException()
 
+			System.out.println("請輸入分母");
+			int m = sc.nextInt();
+			System.out.println("x["+n+"]/"+m+"="+x[n]/m); // new ArithmeticException()
 			for(int i=1;i<=10;i++)
 			{
 				System.out.println("i="+i+"\thello");
 			}
 		}
+		catch(RuntimeException e) { // e = new ArithmeticException
+			System.out.println("重填");
+			e.printStackTrace();
+		}
+		/*
+		 * Unreachable catch block for ArrayIndexOutOfBoundsException. 
+		 * It is already handled by the catch block for RuntimeException
+		 
 		catch(ArrayIndexOutOfBoundsException e)//ArrayIndexOutOfBoundsException e=new ArrayIndexOutOfBoundsException()
 		{
 			System.out.println("索引碼為0~2");
 			e.printStackTrace();
 
 		}
+		
 		catch(InputMismatchException e)
 		{
 			System.out.println("索引碼須為整數");
 			e.printStackTrace();
 		}
+		catch(ArithmeticException e) {
+			System.out.println("分母不可為0");
+			e.printStackTrace();
+		}
+		*/
 		
 	}
 
